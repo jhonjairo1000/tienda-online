a95180429fac6674ba7a7c9e2e1ea866346b69e3
jhonjairo1000 <jhonjairo1000@gmail.com>
2024-11-29 21:22:02 -0500

    actualizacion script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,12 @@ let cart = JSON.parse(localStorage.getItem('cart')) || [];
  
 // Manejo de productos dinámico
 const products = [
-    { id: 1, name: "Laptop", price: 800, img: "pantalla-de-portatil.png" },
-    { id: 2, name: "Smartphone", price: 600, img: "smartphone.jpg" },
-    { id: 3, name: "Sofá", price: 300, img: "sofa.jpg" },
-    { id: 4, name: "Lámpara", price: 50, img: "lamp.jpg" },
-    { id: 5, name: "Bicicleta", price: 150, img: "bike.jpg" },
-    { id: 6, name: "Balón de fútbol", price: 30, img: "football.jpg" },
+    { id: 1, name: "Laptop", price: 800, img: "ordenador-portatil.png" },
+    { id: 2, name: "Smartphone", price: 600, img: "smartphone.png" },
+    { id: 3, name: "Sofá", price: 300, img: "cama.png" },
+    { id: 4, name: "Lámpara", price: 50, img: "lampara-de-mesa.png" },
+    { id: 5, name: "Bicicleta", price: 150, img: "bmx.png" },
+    { id: 6, name: "Balón de fútbol", price: 30, img: "futbol-americano.png" },
   ];
  
   // Agregar producto al carrito
